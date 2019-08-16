c0d2f01e15b7cc1492bc2e6fdc0588cdf26ce7d3
zuleimin <zuleimin@qq.com>
2019-08-16 19:49:39 +0800

    cc

--- ex1.py
+++ ex1.py
@@ -2,3 +2,4 @@ a=3
 b=2
 c=a+b
 print(c)
+print(c*2)
--- practise/testtensorflow.py
+++ practise/testtensorflow.py
@@ -1,7 +1,23 @@
 import tensorflow as tf
+import numpy as np
+from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+%matplotlib inline
 
-a = tf.random_normal((100, 100))
-b = tf.random_normal((100, 500))
-c = tf.matmul(a, b)
-sess = tf.InteractiveSession()
-print(sess.run(c))
+
+trainsamples = 200
+testsamples = 60
+
+# 这里我们定义模型。这个模型中含有一个简单的输入层和一个隐藏的sigmoid激活层。
+def model(X, hidden_weights1, hidden_bias1, ow):
+    hidden_layer =  tf.nn.sigmoid(tf.matmul(X, hidden_weights1)+ b)
+    return tf.matmul(hidden_layer, ow)
+
+# 随机生成数据
+dsX = np.linspace(-1, 1, trainsamples + testsamples).transpose() # 在-1到1内返回均匀间隔的数字
+dsY = 0.4* pow(dsX,2) +2 * dsX + np.random.randn(*dsX.shape) * 0.22 + 0.8 # 生成Y方向的值
+
+plt.figure() # 创建输出文件
+plt.title('Original data')
+plt.scatter(dsX,dsY) # 绘制数据点的散点图
+
